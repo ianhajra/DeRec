e4eade71924eda1e33f102d0dc4c793ba4123759
Ian Hajra <ian_hajra@brown.edu>
2024-10-19 14:40:02 -0400

    Install Process Updates

---
+++ README.md
@@ -0,0 +1,1 @@
+# ReDec: Record Deidentification 
---
+++ build/CMakeFiles/remove_identifiers.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/remove_identifiers.dir/src/deidentify.cpp.o"
+  "CMakeFiles/remove_identifiers.dir/src/deidentify.cpp.o.d"
+  "CMakeFiles/remove_identifiers.dir/src/main.cpp.o"
+  "CMakeFiles/remove_identifiers.dir/src/main.cpp.o.d"
+  "CMakeFiles/remove_identifiers.dir/src/utils.cpp.o"
+  "CMakeFiles/remove_identifiers.dir/src/utils.cpp.o.d"
+  "remove_identifiers"
+  "remove_identifiers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/remove_identifiers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
